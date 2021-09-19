f7fad2305e2e565b4b47403049e69bd8e1df65af
sriegodedios <shaner261996@gmail.com>
2021-09-19 18:54:50 -0500

    Added SQL Queries

---
+++ SQL Queries/SetUpDatabase.sql
@@ -0,0 +1,25 @@
+DROP TABLE IF EXISTS General.Series;
+
+CREATE TABLE General.Series(
+    Series_Id UNIQUEIDENTIFIER PRIMARY KEY default NEWID(),
+    Series_Name VARCHAR(255) NOT NULL,
+    Created_At DATETIME DEFAULT CURRENT_TIMESTAMP,
+    Last_Updated DATETIME DEFAULT CURRENT_TIMESTAMP,
+);
+
+DROP TABLE IF EXISTS General.Messages;
+
+CREATE TABLE General.Messages(
+    Message_Id UNIQUEIDENTIFIER PRIMARY KEY default NEWID(),
+    Title VARCHAR(255) NOT NULL,
+    Speaker VARCHAR(255) NOT NULL,
+    Description VARCHAR(500),
+    Series_Id UNIQUEIDENTIFIER FOREIGN KEY REFERENCES General.Series(Series_Id) NOT NULL,
+    Created_At DATETIME DEFAULT CURRENT_TIMESTAMP,
+    Last_Updated DATETIME DEFAULT CURRENT_TIMESTAMP,
+);
+
+INSERT INTO General.Series VALUES (default,'Versus',default,default)
+
+SELECT * FROM General.Series;
+
